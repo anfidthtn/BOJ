8306830efd706b5bd570617ed39abf1d52062c0b
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-04-20 11:05:45 +0900

    [Bronze III] Title: A+B - 4, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -0,0 +1,9 @@
+import sys
+while True:
+    try:
+        A, B = map(int, sys.stdin.readline().split())
+        print(A+B)
+    except EOFError:
+        break
+    except ValueError:
+        break
