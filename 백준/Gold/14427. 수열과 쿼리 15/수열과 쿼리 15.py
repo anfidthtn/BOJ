ac08d2ef246d353156567f03ca2d6b6a2201ca6a
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-04-20 11:28:52 +0900

    [Gold II] Title: 수열과 쿼리 15, Time: 2832 ms, Memory: 51132 KB -BaekjoonHub

---
+++ 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.py
@@ -0,0 +1,78 @@
+import sys
+
+MAXNUM = 10 ** 9
+
+N = int(sys.stdin.readline())
+nums = [MAXNUM] + list(map(int, sys.stdin.readline().split())) + [MAXNUM]
+
+segTree = [N + 1 for _ in range(N * 4)]
+
+def initMinIdx(nodeNum, start, end, linearIdx):
+    if linearIdx < start or end < linearIdx:
+        return
+    
+    if nums[linearIdx] == nums[segTree[nodeNum]]:
+        segTree[nodeNum] = min(segTree[nodeNum], linearIdx)
+    elif nums[linearIdx] < nums[segTree[nodeNum]]:
+        segTree[nodeNum] = linearIdx
+    
+    if start != end:
+        initMinIdx(nodeNum * 2, start, int((start + end) / 2), linearIdx)
+        initMinIdx(nodeNum * 2 + 1, int((start + end) / 2) + 1, end, linearIdx)
+
+for i in range(1, N + 1):
+    initMinIdx(1, 1, N, i)
+
+def getMinIdx(nodeNum, start, end, left, right):
+    if right < start or end < left:
+        return N + 1
+    
+    if left <= start and end <= right:
+        return segTree[nodeNum]
+    
+    leftMinIdx = getMinIdx(nodeNum * 2, start, int((start + end) / 2), left, right)
+    rightMinIdx = getMinIdx(nodeNum * 2 + 1, int((start + end) / 2) + 1, end, left, right)
+
+    if nums[leftMinIdx] == nums[rightMinIdx]:
+        if leftMinIdx > rightMinIdx:
+            return rightMinIdx
+        else:
+            return leftMinIdx
+    elif nums[leftMinIdx] < nums[rightMinIdx]:
+        return leftMinIdx
+    else:
+        return rightMinIdx
+
+def getLeafIdx(nodeNum, start, end, linearIdx):
+    if linearIdx < start or end < linearIdx:
+        return
+    if start == end:
+        return nodeNum
+    if linearIdx <= int((start + end) / 2):
+        return getLeafIdx(nodeNum * 2, start, int((start + end) / 2), linearIdx)
+    else:
+        return getLeafIdx(nodeNum * 2 + 1, int((start + end) / 2) + 1, end, linearIdx)
+
+def updateMinIdx(nodeNum):
+    nodeNum = int(nodeNum / 2)
+    if nodeNum < 1:
+        return
+
+    if nums[segTree[nodeNum * 2]] == nums[segTree[nodeNum * 2 + 1]]:
+        segTree[nodeNum] = min(segTree[nodeNum * 2], segTree[nodeNum * 2] + 1)
+    elif nums[segTree[nodeNum * 2]] < nums[segTree[nodeNum * 2 + 1]]:
+        segTree[nodeNum] = segTree[nodeNum * 2]
+    else:
+        segTree[nodeNum] = segTree[nodeNum * 2 + 1]
+
+    updateMinIdx(nodeNum)
+
+M = int(sys.stdin.readline())
+
+for _ in range(M):
+    order = list(map(int, sys.stdin.readline().split()))
+    if order[0] == 1:
+        nums[order[1]] = order[2]
+        updateMinIdx(getLeafIdx(1, 1, N, order[1]))
+    else:
+        print(segTree[1])
