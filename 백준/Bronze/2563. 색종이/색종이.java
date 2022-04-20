443dc925dcb1e2df3513cdb5359d5833dff5b11a
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-04-20 10:22:12 +0900

    [Bronze I] Title: 색종이, Time: 76 ms, Memory: 11580 KB -BaekjoonHub

---
+++ 백준/Bronze/2563. 색종이/색종이.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 직사각형 겹치는 거 제한 큰 문제 높은 난이도 문제에서 있긴 한데 이 문제는 제한이 낮으니 그냥 생각하기 편한대로 구현
+ */
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 색칠 되었는지 여부를 저장할 것을 만들고, (초기값 false다.)
+		boolean[][] isColored = new boolean[100][100];
+		
+		// 카운팅할 변수 만들고
+		int countColor = 0;
+		int n = Integer.parseInt(br.readLine());
+		// 색종이(paper)개수만큼 돌린다.
+		for (int p = 0; p < n; p++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			// 입력에서 시작지점을 받는다.
+			int stRow = Integer.parseInt(st.nextToken());
+			int stCol = Integer.parseInt(st.nextToken());
+			// 시작지점에서 10행, 10열을 칠하는데,
+			for(int i = 0; i < 10; i++) {
+				for(int j = 0; j < 10; j++) {
+					// 칠해지지 않은 곳만 칠한다.
+					if (isColored[stRow + i][stCol + j] == false) {
+						// 칠하면서
+						isColored[stRow + i][stCol + j] = true;
+						// 칠한 횟수를 올린다.
+						countColor++;
+					}
+				}
+			}
+		}
+		System.out.print(countColor);
+	}
+}
