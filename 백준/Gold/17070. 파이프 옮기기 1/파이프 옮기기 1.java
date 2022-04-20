9da6ba12596dd07f7da228acb5a006be189db281
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-04-20 11:42:49 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 76 ms, Memory: 11780 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		/**
+		 * 0 : 가로
+		 * 1 : 세로
+		 * 2 : 대각선
+		 */
+		int[][][] cases = new int[N + 1][N + 1][3];
+		boolean[][] map = new boolean[N + 1][N + 1];
+		for(int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j  = 1; j <= N; j++) {
+				if(st.nextToken().charAt(0) == '1') {
+					map[i][j] = true;
+				}
+			}
+		}
+		cases[1][2][0] = 1;
+		
+		for(int row = 1; row <= N; row++) {
+			for(int col = 1; col <= N; col++) {
+				if (map[row][col]) continue;
+				if (!map[row - 1][col]) {
+					cases[row][col][1] += cases[row - 1][col][1] + cases[row - 1][col][2];
+				}
+				if (!map[row][col - 1]) {
+					cases[row][col][0] += cases[row][col - 1][0] + cases[row][col - 1][2];
+				}
+				if (!map[row - 1][col] && !map[row][col - 1]) {
+					cases[row][col][2] += cases[row - 1][col - 1][0] + cases[row - 1][col - 1][1] + cases[row - 1][col - 1][2];
+				}
+			}
+		}
+		
+		System.out.println(cases[N][N][0] + cases[N][N][1] + cases[N][N][2]);
+		
+	}
+}
