b036bbfdc9b47ac2dee13d573b9b28f985f0783f
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-04-20 10:35:57 +0900

    [Bronze V] Title: R2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", 2*b - a);
+    
+    
+    return 0;
+}
