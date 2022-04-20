3154fd26f5de6598294600438ae9bcd45e259138
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-04-20 10:20:33 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 76 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(1, n):
+    for j in range(i):
+        print('*', end='')
+    print()
+for i in range(n, 0, -1):
+    for j in range(i):
+        print('*', end='')
+    print()
