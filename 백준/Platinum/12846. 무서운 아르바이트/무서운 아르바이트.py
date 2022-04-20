da09457dd6481f105d2fe34aaffe04fe7d8e82de
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-04-20 11:23:03 +0900

    [Platinum V] Title: 무서운 아르바이트, Time: 184 ms, Memory: 40364 KB -BaekjoonHub

---
+++ 백준/Platinum/12846. 무서운 아르바이트/무서운 아르바이트.py
@@ -0,0 +1,29 @@
+import sys
+n = int(sys.stdin.readline())
+ti = list(map(int, sys.stdin.readline().split()))
+
+if n == 1:
+    print(ti[0])
+    exit()
+
+maxEarn = 0
+info = [[ti[0], 0]]
+
+for i in range(1, n):
+    if len(info) > 0 and ti[i] < info[-1][0]:
+        while len(info) > 1 and ti[i] <= info[-2][0]:
+            nowInfo = info.pop()
+            earn = nowInfo[0] * (i - nowInfo[1])
+            maxEarn = max(maxEarn, earn)
+        if ti[i] < info[-1][0]:
+            earn = info[-1][0] * (i - info[-1][1])
+            maxEarn = max(maxEarn, earn)
+            info[-1][0] = ti[i]
+    else:
+        info.append([ti[i], i])
+while len(info) > 0:
+    nowInfo = info.pop()
+    earn = nowInfo[0] * (n - nowInfo[1])
+    maxEarn = max(maxEarn, earn)
+print(maxEarn)
+
