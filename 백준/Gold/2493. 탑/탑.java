5f1e8fd6a48ad585be1b83bf985dd3f09f529532
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-05-12 09:26:33 +0900

    [Gold V] Title: 탑, Time: 604 ms, Memory: 107224 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 /**
@@ -20,20 +19,24 @@ public class Main {
 		 * 새로 들어온 녀석이 top에 있는 높이보다 높으면 top을 줄임 (top이 -1이 되거나 높은 녀석이 나올 때까지)
 		 * 이 때, -1이 되면 0을 출력하고 top을 증가(0으로)시키고 현재 높이, 위치 저장
 		 */
-		Stack<Integer[]> stack = new Stack<>();
+		int[][] stack = new int[n + 1][2];
+		int top = -1;
 		for (int i = 0; i < n; i++) {
 			int height = Integer.parseInt(st.nextToken());
 			// 스택이 다 비거나, 더 높은 높이가 나올 때까지 top을 줄임
-			while (!stack.isEmpty() && stack.lastElement()[0] < height) stack.pop();
-			if (stack.isEmpty()) {
+			while (top >= 0 && stack[top][0] < height) top--;
+			if (top == -1) {
 				// 수신지점이 없으니 0
 				sb.append("0 ");
 			}
 			else {
 				// 수신지점이 있으니 수신지점 출력 (인덱스 + 1)
-				sb.append(stack.lastElement()[1] + 1).append(" ");
+				sb.append(stack[top][1] + 1);
+				sb.append(" ");
 			}
-			stack.push(new Integer[] {height, i});
+			top++;
+			stack[top][0] = height;
+			stack[top][1] = i;
 		}
 		System.out.print(sb.toString());
 	}
