2e9d34d77eba4f748a246558bdb8a7dbf4c6a66c
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-06-13 23:17:27 +0900

    [Bronze IV] Title: 욱 제, Time: 84 ms, Memory: 11856 KB -BaekjoonHub

---
+++ 백준/Bronze/17356. 욱 제/욱 제.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		System.out.print(String.format("%.10f", 1.0 / (1.0 + Math.pow(10, (B - A) / 400.0))));
+	}
+}
