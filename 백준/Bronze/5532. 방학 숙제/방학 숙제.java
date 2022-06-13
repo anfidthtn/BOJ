71c7c4a68a356ec148e774d9165a7000c0b93ec2
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-06-13 09:00:10 +0900

    [Bronze IV] Title: 방학 숙제, Time: 76 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int L = Integer.parseInt(br.readLine());
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+		int D = Integer.parseInt(br.readLine());
+		
+		System.out.println((int)(L - Math.max(Math.ceil((double) A / C), Math.ceil((double) B / D))));
+	}
+}
