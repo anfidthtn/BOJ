f9ec7446da4cbd74c834bfbcd9c986f37d3c1aa1
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-06-20 23:30:34 +0900

    [Bronze IV] Title: 과자, Time: 72 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		System.out.println(Math.max(K * N - M, 0));
+	}
+}
