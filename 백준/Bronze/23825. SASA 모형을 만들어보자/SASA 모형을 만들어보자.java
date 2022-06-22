f4616c55e3640dce2345acd84ba20e421453cd2e
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-06-22 22:31:42 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 76 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		System.out.println(Math.min(n / 2, m / 2));
+	}
+}
