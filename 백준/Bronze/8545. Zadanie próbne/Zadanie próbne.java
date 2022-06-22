7b08ec443e9cbcdcb9b19c0dcdc5ce35168daafa
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-06-22 22:20:31 +0900

    [Bronze V] Title: Zadanie próbne, Time: 72 ms, Memory: 11468 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		System.out.print(str.charAt(2));
+		System.out.print(str.charAt(1));
+		System.out.print(str.charAt(0));
+	}
+}
