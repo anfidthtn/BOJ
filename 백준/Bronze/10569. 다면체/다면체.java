898bf4c9d30f6ff039277b2813898f5e8753de89
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-06-23 23:23:02 +0900

    [Bronze III] Title: 다면체, Time: 72 ms, Memory: 11328 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int tNum = 1; tNum <= t; tNum++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int V = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
+			sb.append(2 + E - V).append("\n");
+		}
+		System.out.print(sb.toString());
+	}
+}
