58a16f3df41bc63823b0fda0ee43556aa6db9cca
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-06-29 19:47:15 +0900

    [Gold III] Title: 과제, Time: 96 ms, Memory: 12132 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/README.md
+++ 백준/Gold/13904. 과제/README.md
@@ -4,11 +4,11 @@
 
 ### 성능 요약
 
-메모리: 12172 KB, 시간: 96 ms
+메모리: 12132 KB, 시간: 96 ms
 
 ### 분류
 
-자료 구조(data_structures), 그리디 알고리즘(greedy), 우선순위 큐(priority_queue)
+자료 구조(data_structures), 그리디 알고리즘(greedy), 우선순위 큐(priority_queue), 정렬(sorting)
 
 ### 문제 설명
 
--- 백준/Gold/13904. 과제/과제.java
+++ 백준/Gold/13904. 과제/과제.java
@@ -2,47 +2,56 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Node implements Comparable<Node>{
+	static class Task {
 		int date;
 		int score;
-		public Node(int date, int score) {
-			super();
+
+		public Task(int date, int score) {
 			this.date = date;
 			this.score = score;
 		}
-		@Override
-		public int compareTo(Node o) {
-			if (score - o.score == 0) {
-				return o.date - date;
-			}
-			return o.score - score;
-		}
 	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		List<Node>[] list = new ArrayList[1001];
-		for(int i = 1; i <= 1000; i++) {
-			list[i] = new ArrayList<>();
-		}
-		for(int i = 0; i < N; i++) {
+		List<Task> tasks = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int date = Integer.parseInt(st.nextToken());
-			list[date].add(new Node(date, Integer.parseInt(st.nextToken())));
+			tasks.add(new Task(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 		}
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		int score = 0;
-		for(int date = 1000; date >= 1; date--) {
-			pq.addAll(list[date]);
-			if(!pq.isEmpty()) {
-				score+=pq.poll().score;
+		// 일자에 대한 내림차순
+		tasks.sort(new Comparator<Task>() {
+			@Override
+			public int compare(Task o1, Task o2) {
+				return o2.date - o1.date;
+			}
+		});
+
+		// 점수에 대한 내림차순
+		PriorityQueue<Task> pq = new PriorityQueue<>(new Comparator<Task>() {
+			@Override
+			public int compare(Task o1, Task o2) {
+				return o2.score - o1.score;
+			}
+		});
+
+		int scoreSum = 0;
+		int idx = 0;
+		for (int day = 1000; day >= 1; day--) {
+			while (idx < N && tasks.get(idx).date >= day) {
+				pq.add(tasks.get(idx++));
+			}
+			if (!pq.isEmpty()) {
+				scoreSum += pq.poll().score;
 			}
 		}
-		System.out.println(score);
+		System.out.println(scoreSum);
 	}
 }
