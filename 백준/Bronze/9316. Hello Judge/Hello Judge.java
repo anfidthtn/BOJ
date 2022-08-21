f9428714ef01c3c353c1b260f9b7f68f16ef363e
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-08-21 23:29:35 +0900

    [Bronze III] Title: Hello Judge, Time: 72 ms, Memory: 11348 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int tNum = 1; tNum <= t; tNum++) {
+			sb.append("Hello World, Judge ").append(tNum).append("!\n");
+		}
+		System.out.print(sb.toString());
+	}
+}
