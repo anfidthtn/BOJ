62c03e1453d649dc37dbeed5e413ed8e577fd091
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-08-21 19:11:42 +0900

    [Bronze III] Title: 윷놀이, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -0,0 +1,12 @@
+for _ in range(3):
+  s = sum(map(int, input().split()))
+  if s == 0:
+    print('D')
+  if s == 1:
+    print('C')
+  if s == 2:
+    print('B')
+  if s == 3:
+    print('A')
+  if s == 4:
+    print('E')
