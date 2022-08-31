abb55f00c65cafb0d9306d1c885bef4c100e1481
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-08-31 22:19:32 +0900

    [Silver III] Title: N과 M (4), Time: 100 ms, Memory: 13676 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static StringBuilder sb;
+	static int N;
+	static int M;
+	static int[] selected;
+//	static boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		selected = new int[M];
+//		visited = new boolean[N + 1];
+		recur(0, 1);
+		System.out.print(sb.toString());
+	}
+
+	public static void recur(int cur, int start) {
+		if (cur == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(selected[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = start; i <= N; i++) {
+//			if (visited[i]) {
+//				continue;
+//			}
+//			visited[i] = true;
+			selected[cur] = i;
+			recur(cur + 1, i);
+//			visited[i] = false;
+		}
+	}
+}
