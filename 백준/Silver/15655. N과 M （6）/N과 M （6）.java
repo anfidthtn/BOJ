4e311c803606199767cda68fd00b8904c509b7fe
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2022-08-31 22:22:50 +0900

    [Silver III] Title: N과 M (6), Time: 236 ms, Memory: 18760 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class Main {
+	static StringBuilder sb;
+	static int N;
+	static int[] nums;
+	static int M;
+	static int[] selected;
+//	static boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		selected = new int[M];
+		nums = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		Arrays.sort(nums);
+//		visited = new boolean[N + 1];
+		recur(0, 0);
+		System.out.print(sb.toString());
+	}
+
+	public static void recur(int cur, int start) {
+		if (cur == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(selected[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = start; i < N; i++) {
+//			if (visited[i]) {
+//				continue;
+//			}
+//			visited[i] = true;
+			selected[cur] = nums[i];
+			recur(cur + 1, i + 1);
+//			visited[i] = false;
+		}
+	}
+}
