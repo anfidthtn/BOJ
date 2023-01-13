8598ec4da915a2a11a8650325ea74bd462c52733
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2023-01-13 15:34:29 +0900

    [Gold V] Title: 4와 7, Time: 112 ms, Memory: 12808 KB -BaekjoonHub

--- 백준/Gold/2877. 4와 7/4와 7.java
+++ 백준/Gold/2877. 4와 7/4와 7.java
@@ -1,1 +1,1 @@
-import java.util.*;class Main{public static void main(String[]a){int K=new Scanner(System.in).nextInt(),x=1;for(;K>x*2;K-=x*=2);K--;for(;x>0;x/=2){System.out.print((K&x)>0?7:4);}}}
+import java.util.*;class Main{public static void main(String[]a){int K=new Scanner(System.in).nextInt(),x=1;for(;K>x*2;K-=x*=2);K--;for(;x>0;x/=2)System.out.print((K&x)>0?7:4);}}
