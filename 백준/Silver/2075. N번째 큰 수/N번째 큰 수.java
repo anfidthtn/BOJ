23ed0b09b08615aa0536c708bbbed7f986f8a99a
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2023-01-19 17:08:34 +0900

    [Silver II] Title: N번째 큰 수, Time: 1036 ms, Memory: 311932 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] nums = new int[N * N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				nums[i * N + j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		Arrays.sort(nums);
+		System.out.println(nums[N * N - N]);
+	}
+}
