1556eeae35a82a03899db5e08230ac919321ee81
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2023-04-27 23:55:19 +0900

    [Silver V] Title: 2021은 무엇이 특별할까?, Time: 76 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Silver/24039. 2021은 무엇이 특별할까？/2021은 무엇이 특별할까？.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+				101, 103 };
+		int i = 0;
+		while (primes[i] * primes[i + 1] <= N) {
+			i++;
+		}
+		System.out.print(primes[i] * primes[i + 1]);
+	}
+}
