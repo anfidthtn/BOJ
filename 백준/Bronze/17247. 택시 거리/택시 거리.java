4007735aef772adf313e197f4434e4252bf4ad41
anfidthtn <44693731+anfidthtn@users.noreply.github.com>
2023-04-28 16:57:24 +0900

    [Bronze I] Title: 택시 거리, Time: 364 ms, Memory: 53880 KB -BaekjoonHub

---
+++ 백준/Bronze/17247. 택시 거리/택시 거리.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		List<int[]> list = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				if (Integer.parseInt(st.nextToken()) == 1) {
+					list.add(new int[] { i, j });
+				}
+			}
+		}
+		System.out.print(Math.abs(list.get(0)[0] - list.get(1)[0]) + Math.abs(list.get(0)[1] - list.get(1)[1]));
+	}
+}
